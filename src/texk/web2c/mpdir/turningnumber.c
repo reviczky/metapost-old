66938cb3e3ef62a2cb2af9085a763d905a290484
taco <taco@b8f95e4b-c8ec-496e-911b-43368900ac28>
2006-11-03 22:13:15 +0000

    cleaned up a bit

--- src/texk/web2c/mpdir/turningnumber.c
+++ src/texk/web2c/mpdir/turningnumber.c
@@ -2,17 +2,16 @@
 
 #include "mplib.h"
 
-/* return more than 360 signal an err */
-
 #define bezier_error (720<<20)+1
 
 #define sign(v) ((v)>0 ? 1 : ((v)<0 ? -1 : 0 ))
 
-#define print_roots(a)  fprintf(stdout,"\n%s, angles = %d,%d",(a),(xi)>>20,(xo)>>20)
+#define print_roots(a) fprintf(stdout,"\n%s, i=%d,o=%d",(a),(xi)>>20,(xo)>>20)
 
 angle 
-bezierslope(integer AX,integer AY,integer BX,integer BY,integer CX,integer CY,integer DX,integer DY) {
-  long long a,b,c,diff;
+bezierslope(integer AX,integer AY,integer BX,integer BY,
+            integer CX,integer CY,integer DX,integer DY) {
+  long long a,b,c;
   long long ax=AX,ay=AY,bx=BX,by=BY,cx=CX,cy=CY,dx=DX,dy=DY;
   angle xi = 0, xo = 0, res = 0;
   integer deltax,deltay;
@@ -29,7 +28,9 @@ bezierslope(integer AX,integer AY,integer BX,integer BY,integer CX,integer CY,in
 
   assert(sizeof(long long)>=8);
 
-  fprintf(stdout,"\nargs: (%lld,%lld),(%lld,%lld),(%lld,%lld),(%lld,%lld)",ax,ay,bx,by,cx,cy,dx,dy);
+  fprintf(stdout,
+    "\nargs: (%lld,%lld),(%lld,%lld),(%lld,%lld),(%lld,%lld)",
+             ax,ay,bx,by,cx,cy,dx,dy);
   
   a = ((bx-ax)*(cy-by)) - ((cx-bx)*(by-ay)); /* a = (bp-ap)x(cp-bp); */
   c = ((cx-bx)*(dy-cy)) - ((dx-cx)*(cy-by)); /* c = (cp-bp)x(dp-cp);*/
