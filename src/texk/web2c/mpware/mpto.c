480ae733b190b7b8ddac920efec073112f5bca4c
taco <taco@b8f95e4b-c8ec-496e-911b-43368900ac28>
2007-08-11 08:41:03 +0000

    fix for labels without  trailing whitespace

--- src/texk/web2c/mpware/mpto.c
+++ src/texk/web2c/mpware/mpto.c
@@ -25,9 +25,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifdef WIN32
 #include <string.h>
-#endif
 
 
 /* MetaPost itself has a configurable max line length, but we can afford to
@@ -49,7 +47,7 @@ char *tex_pretex1 = "\\gdef\\mpxshipout{\\shipout\\hbox\\bgroup%\n"
     "  \\ht0=0pt \\dp0=0pt \\box0 \\egroup}\n"
     "\\mpxshipout%% line %d %s\n";
 char *tex_pretex = "\\mpxshipout%% line %d %s\n";
-char *tex_posttex = "\\stopmpxshipout\n";
+char *tex_posttex = "\n\\stopmpxshipout\n";
 char *tex_preverb1 = "";	/* if very first instance */
 char *tex_preverb = "%% line %d %s\n";	/* all other instances */
 char *tex_postverb = "%\n";
@@ -300,23 +298,14 @@ copytex(void)
 {
     char *s;			/* where a string to print stops */
     char c;
-
-    while (*aa == ' ' || *aa == '\t')
-	aa++;
-    if (*aa == 0)
-	if ((aa = getline()) == NULL)
-	    err("btex section does not end");
+    char *res = NULL;
     do {
 	if (*aa == 0)
-	    if ((aa = getline()) == NULL)
-		err("btex section does not end");
-	    else
-		printf("\n");
+	  if ((aa = getline()) == NULL)
+	    err("btex section does not end");
+
 	if (getbta(aa) && *tt == 'e') {
-	    s = tt - 1;
-	    while (s >= bb && (*s == ' ' || *s == '\t'))
-		s--;
-	    s++;
+     	    s = tt;
 	} else {
 	    if (*tt == 'b')
 		err("btex in TeX mode");
@@ -326,9 +315,37 @@ copytex(void)
 	}
 	c = *s;
 	*s = 0;
-	printf("%s", bb);
+	if (res==NULL) {
+	  res = malloc(strlen(bb)+1);
+	  if (res==NULL)
+	    err("memory allocation failure");
+	  res = strncpy(res,bb,(strlen(bb)+1));
+	} else {
+	  res = realloc(res,strlen(res)+strlen(bb)+2);
+	  if (res==NULL)
+	    err("memory allocation failure");
+	  res = strncat(res,"\n",1);
+	  res = strncat(res,bb, strlen(bb));
+	}
 	*s = c;
     } while (*tt != 'e');
+    /* whitespace at the end */
+    s = res+strlen(res)-1;
+    if ((*s == ' ' || *s == '\t' || *s == '\r'|| *s == '\n')) {
+      while (s >= res && (*s == ' ' || *s == '\t' || *s == '\r'|| *s == '\n'))
+	s--;
+      if (s!=(res+strlen(res)-1)) 
+	s++;
+    } else {
+      s++;
+    }
+    *s=0;
+    /* whitespace at the start */
+    s =res;
+    while (s<(res+strlen(res)) && (*s == ' ' || *s == '\t' || *s == '\r'|| *s == '\n'))
+      s++;
+    printf("%s%%",s);
+    free(res);
 }
 
 
@@ -386,7 +403,7 @@ main(int argc, char **argv)
 	exit(0);
     } else if (argc > 1 && (strcmp(argv[1], "--version") == 0
 			    || strcmp(argv[1], "-version") == 0)) {
-	printf("mpto 1.000\n\
+	printf("mpto 1.001\n\
 This program is in the public domain.\n\
 Primary author of mpto: John Hobby.\n\
 Current maintainer: Taco Hoekwater.\n");
